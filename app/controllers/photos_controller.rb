bd0961c53d35909779287e27d9839665da4a8d01
Natalia Botia <nbotia@users.noreply.github.com>
2016-10-31 16:46:17 -0500

    Second changes

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -15,7 +15,6 @@ class PhotosController < ApplicationController
   def new_form
     render("new_form.html.erb")
   end
-
 # Im able to add a new picture
   def create_row
     p = Photo.new
@@ -23,6 +22,26 @@ class PhotosController < ApplicationController
     p.source = params[:the_source]
     p.save
     # render("create_row.html.erb")
+    redirect_to("http://localhost:3000")
+  end
+
+  def destroy
+    @photo = Photo.find_by({:id=>params[:id]})
+    @photo.destroy
+    redirect_to("http://localhost:3000")
+  end
+
+  def edit_form
+    @photo = Photo.find_by({:id=>params[:id]})
+    render("edit_form.html.erb")
+  end
+
+  def update_row
+    @photo2 = Photo.find_by({:id=>params[:id]})
+    @photo2.caption = params[:the_caption2]
+    @photo2.source = params[:the_source2]
+    @photo2.save
+
     redirect_to("http://localhost:3000")
   end
 

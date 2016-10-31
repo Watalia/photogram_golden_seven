990b7f8a43ff612203ad5c517b58e1c103571efe
Natalia Botia <nbotia@users.noreply.github.com>
2016-10-31 15:16:31 -0500

    First set of changes

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,29 @@
 class PhotosController < ApplicationController
+  # params={"source"=>"http://upload.wikimedia.org/wikipedia/commons/thumb/something_Else", "caption"=>"2"}
   def index
-    @list_of_photos = Photo.all
+    @list_of_photos = (Photo.all)
+      render("index.html.erb")
   end
 
+  def show
+    @photo = Photo.find_by({:id=>params[:id]})
+    @photo_source = @photo.source
+    @photo_caption = @photo.caption
+    @photo_id = @photo.id
+  end
+
+  def new_form
+    render("new_form.html.erb")
+  end
+
+# Im able to add a new picture
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+    # render("create_row.html.erb")
+    redirect_to("http://localhost:3000")
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
-
+  get("/photos/new", { :controller => "photos", :action => "new_form" })
+  get("/create_photo",    { :controller => "photos", :action => "create_row" })
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
 
+  get("/temp",              {:controller => "photos", :action=>"temp"})
 end
